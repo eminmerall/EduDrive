c0a57966df5645f4de208ddbe74fbcf424d36efe
eminmerall <eminmerall@gmail.com>
2023-12-02 19:31:54 +0300

    Dinamik Veri Listeleme

--- DriveApp/Drive/urls.py
+++ DriveApp/Drive/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("files", views.files),
-    path("files/<slug:slug>",views.files_details)
+    path("", views.index, name="home_page"),
+    path("files", views.files, name="files_page"),
+    path("files/<slug:slug>",views.file_details, name="file_details")
 ]
--- DriveApp/Drive/views.py
+++ DriveApp/Drive/views.py
@@ -1,13 +1,86 @@
+from datetime import date
 from django.shortcuts import render
+
+data = {
+    "files": [
+        {
+            "title": "Dosya adı 1",
+            "decription": "Dosya açıklama 1 ",
+            "imageUrl": "m1.jpg",
+            "coverImage": "cover1.jpg",
+            "slug": "file-name-1",
+            "language": "english",
+            "date" : date(2020,5,10)
+        },
+        {
+            "title": "Dosya adı 2",
+            "decription": "Dosya açıklama 2 ",
+            "imageUrl": "m2.jpg",
+            "coverImage": "cover2.jpg",
+            "slug": "file-name-2",
+            "language": "english",
+            "date" : date(2021,6,1)
+        },
+        {
+            "title": "Dosya adı 3",
+            "decription": "Dosya açıklama 3 ",
+            "imageUrl": "m3.jpg",
+            "coverImage": "cover3.jpg",
+            "slug": "file-name-3",
+            "language": "english",
+            "date" : date(2023,7,10)
+        },
+        {
+            "title": "Dosya adı 4",
+            "decription": "Dosya açıklama 4 ",
+            "imageUrl": "m4.jpg",
+            "coverImage": "cover1.jpg",
+            "slug": "file-name-4",
+            "language": "english",
+            "date" : date(2022,8,25)
+        }
+    ],
+    "sliders": [
+        {
+            "slider_image":"slider1.jpg",
+            "url":"file-name-1"
+        },
+        {
+            "slider_image":"slider2.jpg",
+            "url":"file-name-2"
+        },
+        {
+            "slider_image":"slider3.jpg",
+            "url":"file-name-3"
+        }
+    ]
+}
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    files = data["files"][-4:]
+    sliders=data["sliders"]
+    return render(request, 'index.html',{
+        "files": files,
+        "sliders":sliders
+    })
 
 def files(request):
-    return render(request,'files.html')
+    files = data["files"]
+    return render(request,'files.html',{
+        "files": files
+    })
 
-def files_details(request, slug):
+def file_details(request, slug):
+    files = data["files"]
+    # selectedFile = None
+    # for file in files:
+    #    if file["slug"] == slug:
+    #        selectedFile = file
+
+    selectedFile = next(file for file in files if file["slug"]==slug)
+
+
     return render(request,'file-details.html',{
-        "slug":slug
+        "file": selectedFile
     })
